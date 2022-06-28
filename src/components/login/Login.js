5d5f88fbacf4847f4e9d024d46770e77252f4d7e
Njoki <kinuthiash@gmail.com>
2022-06-28 15:05:45 +0300

    style: add sign up styling

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -3,23 +3,53 @@ import './Login.css';
 
 const Login = () => {
 
-  return (
-    <div className='background'>
-      <form className="formContainer login">
-        <h5 className='green'>Login</h5>
-        <div>
-          <div >
-            <input type="text" placeholder="user email"  required />
-          </div>
+  function LogInForm() {
+    return (
+      <div className='background'>
+        <form className="formContainer login">
+          <h5 className='green'>Login</h5>
           <div>
-            <input type="password" placeholder="Password" required />
-          </div>         
-        </div>
-        <p>Don't have an account?<a className="green px-3" href="./" >Sign Up</a></p>
-        <button type="submit" className="loginBtn">Submit</button>
-      </form>
-      
-    </div>
+            <div >
+              <input type="text" placeholder="user email" required />
+            </div>
+            <div>
+              <input type="password" placeholder="Password" required />
+            </div>
+          </div>
+          <p>Don't have an account?<a className="green px-3" href="./" >Sign Up</a></p>
+          <button type="submit" className="loginBtn">Submit</button>
+        </form>
+
+      </div>
+    )
+  }
+
+  function SignUpForm() {
+    return (
+      <div className='background'>
+        <form className="formContainer login">
+          <h5 className='green'>Sign Up</h5>
+          <div>
+            <div >
+              <input type="text" placeholder="Email Address" required />
+            </div>
+            <div>
+              <input type="password" placeholder="Password" required />
+            </div>
+            <div>
+              <input type="password" placeholder="Confirm Password" required />
+            </div>
+          </div>
+          <button type="submit" className="loginBtn">Submit</button>
+        </form>
+
+      </div>
+    )
+  }
+
+  return (
+    // <LogInForm />
+    <SignUpForm/>
   )
 }
 
